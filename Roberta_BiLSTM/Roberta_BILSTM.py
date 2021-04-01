a1b321969e27353fa7f0ff2a8c72175f317541a9
Fionaforever1999 <yuelvji@126.com>
2021-04-01 20:12:47 +0800

    reformat

---
+++ Roberta_BiLSTM/Roberta_BILSTM.py
@@ -0,0 +1,114 @@
+import torch
+import torch.autograd as autograd
+import torch.optim as optim
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+import numpy as np
+from fairseq.models.roberta import RobertaModel
+
+torch.manual_seed(1)
+
+import torch
+import torch.nn as nn
+roberta = RobertaModel.from_pretrained('/Users/apple/PycharmProjects/fairseq/fairseq/models/roberta/roberta.base', checkpoint_file='model.pt')
+roberta.eval()
+
+assert isinstance(roberta.model, torch.nn.Module)
+
+
+
+# how to use roBERTa
+# bpe extract word byte pair encoding
+#
+
+"""
+Sort according to length
+"""
+def sort_batch(data, label, length):
+  batch_size = data.size(0)
+  # 先将数据转化为numpy()，再得到排序的index
+  # first chaneg data intp numpy and sort to get index
+  inx = torch.from_numpy(np.argsort(length.numpy())[::-1].copy())
+  data = data[inx]
+  label = label[inx]
+  length = length[inx]
+  # length转化为了list格式，不再使用torch.Tensor格式
+  # length to transfer to list not using torch.Tesor
+  length = list(length.numpy())
+  return (data, label, length)
+
+
+class BiLSTM(nn.Module):
+  def __init__(self, input_dim, hidden_dim, output_dim, num_layers, biFlag, dropout=0.5):
+    # input_dim 输入特征维度d_input
+    # hidden_dim 隐藏层的大小
+    # output_dim 输出层的大小（分类的类别数）
+    # num_layers LSTM隐藏层的层数
+    # biFlag 是否使用双向
+    super(BiLSTM, self).__init__()
+    self.input_dim = input_dim
+    self.hidden_dim = hidden_dim
+    self.output_dim = output_dim
+    self.num_layers = num_layers
+    if (biFlag):
+      self.bi_num = 2
+    else:
+      self.bi_num = 1
+    self.biFlag = biFlag
+    # 根据需要修改device
+    # change device
+    self.device = torch.device("cuda")
+
+    # define LSTM input output layer_number  batch_first and dropout portion
+    # 定义LSTM网络的输入，输出，层数，是否batch_first，dropout比例，是否双向
+    self.layer1 = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim, \
+                          num_layers=num_layers, batch_first=True, \
+                          dropout=dropout, bidirectional=biFlag)
+    # define output layer and use log_softmax to output
+    # 定义线性分类层，使用logsoftmax输出
+    self.layer2 = nn.Sequential(
+      nn.Linear(hidden_dim * self.bi_num, output_dim),
+      nn.LogSoftmax(dim=2)
+    )
+
+    self.to(self.device)
+
+  def init_hidden(self, batch_size):
+    # define the hidden state
+    return (torch.zeros(self.num_layers * self.bi_num, batch_size, self.hidden_dim).to(self.device),
+            torch.zeros(self.num_layers * self.bi_num, batch_size, self.hidden_dim).to(self.device))
+
+  def forward(self, x, y, length):
+    # input original x, label y and length
+    batch_size = x.size(0)
+    max_length = torch.max(length)
+    # truncked according to the maxium length
+    x = x[:, 0:max_length, :]
+    y = y[:, 0:max_length]
+    x, y, length = sort_batch(x, y, length)
+    x, y = x.to(self.device), y.to(self.device)
+    # pack sequence
+    x = pack_padded_sequence(x, length, batch_first=True)
+
+    # run the network
+    hidden1 = self.init_hidden(batch_size)
+    out, hidden1 = self.layer1(x, hidden1)
+    # out,_=self.layerLSTM(x) is also ok if you don't want to refer to hidden state
+    # unpack sequence
+    out, length = pad_packed_sequence(out, batch_first=True)
+    out = self.layer2(out)
+
+    # return the node and prediction output and length
+    # 返回正确的标签，预测标签，以及长度向量
+    return y, out, length
+
+
+# roberta = RobertaModel.from_pretrained('/path/to/roberta.base', checkpoint_file='model.pt')
+#
+#
+#
+# all_layers = roberta.extract_features(tokens, return_all_hiddens=True)
+# assert len(all_layers) == 25
+# assert torch.all(all_layers[-1] == last_layer_features)
+# # roberta.eval()  # disable dropout (or leave in train mode to finetune)
+
+
---
+++ Roberta_BiLSTM/reformat.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+"""
+Reformat file
+Original format:
+      PMID | t | Title text
+      PMID | a | Abstract text
+      PMID TAB StartIndex TAB EndIndex TAB MentionTextSegment TAB SemanticTypeID TAB EntityID
+New format:
+      Title1 text
+      Abstract1 text 
+      
+      Title2 text
+      Abstract2 text
+      
+"""
+
+
+def produce_source(filename, dev, test, train):
+  abstractList = []
+  titleList = []
+  labelList = []
+
+  dev_nodes = []
+  test_nodes = []
+  train_nodes = []
+  with open(dev, "r") as fdev:
+    for i in fdev.readlines():
+      dev_nodes.append(i.split("\n")[0])
+  with open(test, "r") as ftest:
+    for i in ftest.readlines():
+      test_nodes.append(i.split("\n")[0])
+  with open(train, "r") as ftrain:
+    for i in ftrain.readlines():
+      train_nodes.append(i.split("\n")[0])
+
+  # file = open("source.txt", "w")
+  dev_raw = open("dev.raw", "w")
+  test_raw = open("test.raw","w")
+  train_raw = open("train.raw","w")
+  with open(filename, "r")as f:
+    for i in f.readlines():
+      if "|t|" in i:
+        node_id = i.split("|t|")[0]
+        # print(node_id)
+        title = i.split("|t|")[1]
+        if node_id in dev_nodes:
+          dev_raw.write(title)
+        elif node_id in test_nodes:
+          test_raw.write(title)
+        else:
+          train_raw.write(title)
+
+      # titleList.append(i.split("|t|")[1])
+
+      if "|a|" in i:
+        node_id = i.split("|a|")[0]
+        abstract = i.split("|a|")[1]
+        if node_id in dev_nodes:
+          dev_raw.write(abstract)
+          dev_raw.write("\n")
+        elif node_id in test_nodes:
+          test_raw.write(abstract)
+          test_raw.write("\n")
+        else:
+          train_raw.write(abstract)
+          train_raw.write("\n")
+        # abstractList.append(i.split("|a|")[1])
+
+
+
+      else:
+        labelList.append(i)
+
+
+if __name__ == "__main__":
+  filename = "/Users/apple/PycharmProjects/fairseq/Roberta_BiLSTM/data/MedMentions/corpus_pubtator.txt"
+  dev = "/Users/apple/PycharmProjects/fairseq/Roberta_BiLSTM/data/MedMentions/corpus_pubtator_pmids_dev.txt"
+  test = "/Users/apple/PycharmProjects/fairseq/Roberta_BiLSTM/data/MedMentions/corpus_pubtator_pmids_test.txt"
+  train = "/Users/apple/PycharmProjects/fairseq/Roberta_BiLSTM/data/MedMentions/corpus_pubtator_pmids_trng.txt"
+
+  produce_source(filename, dev, test, train)
